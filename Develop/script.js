e47a8e8f3c488079a5409556a3f242b286535aae
lgenzone <lgenzone@icloud.com>
2022-11-14 16:22:59 -0600

    update index.html & js
    add time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,25 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-// display current date (Today is day of the week, month, day, year)
+// display current date (today is <weekday, month, day, year>)
 var today = dayjs();
 $('#currentDay').text(today.format('[Today is] dddd' + ' MMM D, YYYY'));
 
 
+$(".saveBtn").click(function () {
+  
+  });
 
 
 
-$(function () {
+
+
+
+  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+  // the code isn't run until the browser has finished rendering all the elements
+  // in the html.
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
+
+
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
@@ -29,4 +35,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
